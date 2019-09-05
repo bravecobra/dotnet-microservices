498d64895e5507f3e714857a39ffcacc2ee14197
Brave Cobra <bravecobra@gmail.com>
2019-09-05 09:19:28 +0200

    Removing containers once backup/restore has finished

--- compose/_scripts/consul-restore.ps1
+++ compose/_scripts/consul-restore.ps1
@@ -2,7 +2,6 @@ param(
     [Parameter(Position=0,mandatory=$true)]
     [string] $backupFile
     )
-
 Write-Output "[*] Executing restore..."
 $backup_token = Get-Content ./_data/backup.txt
-docker-compose run consul-backup consul-backup -i consul:8500 -r -t $backup_token  $backupFile
+docker-compose run --rm consul-backup consul-backup -i consul:8500 -r -t $backup_token $backupFile
--- compose/_scripts/consul-restore.sh
+++ compose/_scripts/consul-restore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 echo "[*] Executing restore..."
-docker-compose run backup consul-backup -i consul:8500 -r -t $(grep 'backup_token:' ./_data/keys.txt | awk -v RS='\r\n' '{printf $2}') $1
+docker-compose run --rm backup consul-backup -i consul:8500 -r -t $(grep 'backup_token:' ./_data/keys.txt | awk -v RS='\r\n' '{printf $2}') $1
