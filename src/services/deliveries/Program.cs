42bdd33846bc60de40660adafef24686caef748e
Brave Cobra <bravecobra@gmail.com>
2019-09-12 05:00:22 +0200

    Added seq

--- src/services/deliveries/Program.cs
+++ src/services/deliveries/Program.cs
@@ -1,7 +1,12 @@
-﻿using ConsulConfiguration;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using ConsulConfiguration;
 using Metrics;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace deliveries
 {
@@ -9,7 +14,45 @@ namespace deliveries
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddJsonFile("appsettings.logs.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // Lets make sure that if creating web host fails, we can log that error.
+            var loggerConfiguration = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name);
+
+#if DEBUG
+            // Used to filter out potentially bad data due debugging.
+            // Very useful when doing Seq dashboards and want to remove logs under debugging session.
+            loggerConfiguration.Enrich.WithProperty("DebuggerAttached", Debugger.IsAttached);
+#endif
+            Log.Logger = loggerConfiguration.CreateLogger();
+            try
+            {
+                // In some rare cases, creating web host can fail.
+                // This style of logging increases the chances of logging this issue.
+                Log.Logger.Information("Bootstrapping deliveries app...");
+                using (var host = CreateWebHostBuilder(args).Build())
+                {
+                    host.Run();
+                }
+            }
+            catch (Exception e)
+            {
+                // Happens rarely but when it does, you'll thank me. :)
+                Log.Logger.Fatal(e, "Unable to bootstrap deliveries app.");
+            }
+
+            // Make sure all the log sinks have processed the last log before closing the application.
+            Log.CloseAndFlush();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
@@ -17,7 +60,8 @@ namespace deliveries
             return WebHost.CreateDefaultBuilder(args)
                 .ConfigurePrometheusEndpoint()
                 .ConfigureAppConfiguration((context, builder) => builder.ConfigureConsulSettings())
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSerilog();
         }
     }
 }
